67dd0c454c9033179dcf8c885032cce71d40e1ec
FranciscoMedranoS <pako.medrano27@gmail.com>
2024-10-19 18:36:56 -0600

    Primer commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function AuthLayouyt({ children, }: Readonly<{ children: React.ReactNode; }>) {
+    return (
+        <div className="bg-orange-200 w-screen h-screen overflow-hidden grid">
+            <div className="place-content-center place-self-center placed-item-center text-center">
+                <div className="flex flex-col items-center bottom-10 relative">
+                    <Image
+                        src="/OXXO-Logo.png"
+                        alt="Logo de Ocso"
+                        width={250}
+                        height={0} />
+                </div>
+                {children}
+            </div>
+        </div>
+    )
+}
