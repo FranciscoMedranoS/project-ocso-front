b190ea8c9ea6c384e45319c8aadd3c3e2e3d0d16
FranciscoMedranoS <pako.medrano27@gmail.com>
2024-10-19 23:18:40 -0600

    feat: cookies in SSC

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,25 +1,36 @@
 "use client";
 import { API_URL } from "@/constants";
-import { Button, Input } from "@nextui-org/react";
+import { Button, Input, Spinner } from "@nextui-org/react";
 import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+
+import React, { useState } from "react";
 
 export default function LoginPage() {
+  const [submitting, setSubmtting] = useState(false);
+  const router = useRouter();
   const handleSubmit = async (e: React.FormEvent) => {
+    setSubmtting(true);
     e.preventDefault();
     const formData = new FormData(e.target);
     let authData: any = {};
     authData.userEmail = formData.get("userEmail");
     authData.userPassword = formData.get("userPassword");
-    const { data } = await axios.post(
-      `${API_URL}/auth/login`,
-      { ...authData },
-      {
-        withCredentials: true,
-      }
-    );
+    try {
+      const response = await axios.post(
+        `${API_URL}/auth/login`,
+        { ...authData },
+        {
+          withCredentials: true,
+        }
+      );
+      if (response.status === 201) router.push("/dashboard");
 
+      setSubmtting(false);
+    } catch (e) {
+      setSubmtting(false);
+    }
     return;
   };
   return (
@@ -47,8 +58,8 @@ export default function LoginPage() {
         ></Input>
       </div>
       <div className="text-white flex flex-col items-center gap-2">
-        <Button color="primary" type="submit">
-          Iniciar Sesión
+        <Button color="primary" type="submit" disabled={submitting}>
+          {submitting ? "Enviando..." : "Iniciar sesión"}
         </Button>
         <p>
           ¿No tienes cuenta?{" "}
