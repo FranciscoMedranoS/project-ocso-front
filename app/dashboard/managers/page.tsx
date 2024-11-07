bab9c09135bbd40fa7eb4502d67586dd026b4d72
FranciscoMedranoS <pako.medrano27@gmail.com>
2024-11-07 14:04:49 -0600

    Prueba

--- app/dashboard/managers/page.tsx
+++ app/dashboard/managers/page.tsx
@@ -1,5 +1,12 @@
-const ManagersPage = () =>{
-    return null
+
+import ManagerCards from "./_components/ManagerCards";
+
+const ManagersPage = async () => {
+    return (
+        <div className="w-4/12 h-[90vh] max-h-[90vh] overflow-y-auto">
+            <ManagerCards/>
+        </div>
+    )
 }
 
 export default ManagersPage;
