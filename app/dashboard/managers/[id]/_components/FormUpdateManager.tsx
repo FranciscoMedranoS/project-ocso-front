3675cb4cb225baf7b9094d189ec5550492954f96
FranciscoMedranoS <pako.medrano27@gmail.com>
2024-11-30 08:28:47 -0600

    Referencia

--- app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
+++ app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
@@ -1,29 +1,38 @@
-import updateManager from '@/actions/managers/update';
-import { API_URL } from '@/constants';
-import { Manager } from '@/entities';
-import { Button, Input } from '@nextui-org/react';
-import { authHeaders } from '@/helpers/authHeaders';
-import SelectStore from './SelectStore';
+import updateManager from "@/actions/managers/update";
+import { Manager } from "@/entities";
+import { Button, Input } from "@nextui-org/react";
+import SelectStore from "./SelectStore";
+import { API_URL } from "@/constants";
+import { authHeaders } from "@/helpers/authHeaders";
 
-export default async function FormUpdateManager({manager}: {manager: Manager}) {
-    const updateManagerWithId = updateManager.bind(null, manager.managerId);
-    const responseStores = await fetch(`${API_URL}/locations`, {
-        headers: {
-            ...authHeaders(),
-        }
-    })
-    const stores = await responseStores.json();
-    return(
-        <form action={updateManagerWithId} className='bg-orange-400' rounded-md>
-            <h1>Actualizar Manager</h1>
-            <Input defaultValue={manager.managerFullName} placeholder='Marco Aurelio' name="managerFullName"/>
-            <Input defaultValue={manager.managerEmail} placeholder='manager@ocso.com' name="managerEmail"/>
-            <Input defaultValue={String(manager.managerSalary)} placeholder='1000' name="managerSalary"/>
-            <Input defaultValue={manager.managerPhoneNumber} placeholder='123456789' name="managerPhone"/>
-            <SelectStore stores={stores} defaultStore={manager?.location?.locationId}/>
-            <Button color='primary' type='submit'>
-                Actualizar
-            </Button>
-        </form>
-    )
+export default async function FormUpdateManager({
+  manager,
+}: {
+  manager: Manager;
+}) {
+  const updateManagerWithId = updateManager.bind(null, manager.managerId);
+  const responseStores = await fetch(`${API_URL}/locations`, {
+    method: "GET",
+    headers: { ...authHeaders() },
+  });
+  const stores = await responseStores.json();
+  return (
+    <form
+      action={updateManagerWithId}
+      className="bg-orange-400 rounded-md flex flex-col flex-grow-0 gap-2"
+    >
+      <h1>ActualizarManager</h1>
+      <Input required={true} isRequired label="Nombre completo" defaultValue={manager.managerFullName} placeholder="Marco Aurelio" name="managerFullName"/>
+      <Input required={true} isRequired label="Correo electrónico" defaultValue={manager.managerEmail} placeholder="manager@ocso.com" name="managerEmail"/>
+      <Input required={true} isRequired label="Salario" defaultValue={String(manager.managerSalary)} placeholder="12000" name="managerSalary"/>
+      <Input required={true} isRequired label="Número de teléfono" defaultValue={manager.managerPhoneNumber} placeholder="4421234567" name="managerPhoneNumber"/>
+      <SelectStore
+        stores={stores}
+        defaultStore={manager?.location?.locationId}
+      />
+      <Button color="primary" type="submit">
+        Actualizar
+      </Button>
+    </form>
+  );
 }
