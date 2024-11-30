f1059a4da8ad4ef2668d32c19e0cab396d046833
FranciscoMedranoS <pako.medrano27@gmail.com>
2024-11-30 10:29:06 -0600

    .\.git\

--- app/dashboard/managers/[id]/_components/SelectStore.tsx
+++ app/dashboard/managers/[id]/_components/SelectStore.tsx
@@ -1,20 +1,25 @@
-'use client'
-import { Location } from "@/entities"
-import { Select, SelectItem } from "@nextui-org/react"
+"use client";
+import { Location } from "@/entities";
+import { Select, SelectItem } from "@nextui-org/react";
 
-export default function SelectStore({stores, defaultStore}: {stores: Location[], defaultStore: number}){
-    const disabledStores = stores.map((store: Location)=>{
-        if(store.manager){
-            return String(store.locationId)
-        }
-    }).filter((storeId)=> storeId !== undefined)
-    return(
-        <Select label="Tienda" name = "location" defaultSelectedKeys={defaultStore ? [defaultStore]: undefined} disabledKeys={disabledStores}>
-            {stores.map((store: Location) => (
-                <SelectItem key= {store.locationId}>
-                    {store.locationName}
-                </SelectItem>
-            ))}
-        </Select>
-    )
+export default function SelectStore({
+  stores,
+  defaultStore,
+}: {
+  stores: Location[];
+  defaultStore: number;
+}) {
+  const disabledStores = stores
+    .map((store: Location) => {
+      if (store.manager != undefined) return String(store.locationId);
+    })
+    .filter((storeId) => storeId != undefined);
+  return (
+    <Select label="Tienda" name="location" defaultSelectedKeys={defaultStore ? [defaultStore] : undefined} disabledKeys={disabledStores}
+    >
+      {stores.map((store: Location) => (
+        <SelectItem key={store.locationId}>{store.locationName}</SelectItem>
+      ))}
+    </Select>
+  );
 }
